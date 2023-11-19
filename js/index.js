a49485ac29fbf49adebc1e7e418a0f8b33276b44
andrew <andrew_hoffman262@outlook.com>
2023-11-19 14:29:41 -0700

    completed lab

--- js/index.js
+++ js/index.js
@@ -1,58 +1,55 @@
-const form = document.querySelector('#github-form');
-const searchInput = document.querySelector('#search');
-const userList = document.querySelector('#user-list');
-const reposList = document.querySelector('#repos-list');
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#github-form').addEventListener('submit', function(event) {
+document.querySelector('#github-form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+  const searchValue = document.querySelector('#search').value; 
 
-  const username = searchInput.value.trim();
-
-  if (username === '') {
-    // Handle empty search input
-    return;
-  }
-
-  // Clear previous search results
-  userList.innerHTML = '';
-  reposList.innerHTML = '';
-
-  // Make request to GitHub User Search API
-  fetch(`https://api.github.com/search/users?q=${username}`)
+  fetch(`https://api.github.com/search/users?q=${searchValue}`)
     .then(response => response.json())
     .then(data => {
-      if (data.items.length === 0) {
-        // Handle no search results found
-        return;
-      }
+      console.log(data); 
 
-      // Display user information
-      const user = data.items[0];
-      const userItem = document.createElement('li');
-      userItem.innerHTML = `
-        <a href="${user.html_url}" target="_blank">${user.login}</a>
-        <img src="${user.avatar_url}" alt="${user.login}" width="100">
-      `;
-      userList.appendChild(userItem);
+      const userList = document.querySelector('#user-list');
 
-      // Make request to GitHub User Repos API
-      fetch(`https://api.github.com/users/${user.login}/repos`)
-        .then(response => response.json())
-        .then(reposData => {
-          // Display user repositories
-          reposData.forEach(repo => {
-            const repoItem = document.createElement('li');
-            repoItem.innerHTML = `
-              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            `;
-            reposList.appendChild(repoItem);
-          });
-        })
-        .catch(error => {
-          console.error('Error fetching user repositories:', error);
+      userList.innerHTML = '';
+
+      data.items.forEach(user => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <img src="${user.avatar_url}" alt="${user.login}" width="50" height="50">
+          <a href="${user.html_url}" target="_blank">${user.login}</a>
+        `;
+        userList.appendChild(li);
+
+        li.addEventListener('click', function() {
+          getUserRepos(user.login);
         });
+      });
     })
-    .catch(error => {
-      console.error('Error fetching user:', error);
-    });
+    .catch(error => console.error(error));
+});
+
+function getUserRepos(username) {
+  fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); 
+
+      const reposList = document.querySelector('#repos-list');
+
+      reposList.innerHTML = '';
+
+      data.forEach(repo => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          <p>${repo.description}</p>
+        `;
+        reposList.appendChild(li);
+      });
+    })
+    .catch(error => console.error(error));
+}
+});
 });
