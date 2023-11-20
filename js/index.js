9ff41ce4365223372009a03ea1bf5d81ffe6ff78
andrew <andrew_hoffman262@outlook.com>
2023-11-20 13:19:30 -0700

    completed lab

--- js/index.js
+++ js/index.js
@@ -1,55 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#github-form').addEventListener('submit', function(event) {
-document.querySelector('#github-form').addEventListener('submit', function(event) {
+document.addEventListener("DOMContentLoaded", () => {
+// One function to fetch https://api.github.com/users/octocat/repos
+document.getElementById('github-form').addEventListener('submit', function(event) {
   event.preventDefault(); 
 
-  const searchValue = document.querySelector('#search').value; 
+  const searchValue = document.getElementById('search').value;
 
-  fetch(`https://api.github.com/search/users?q=${searchValue}`)
+  fetch(`https://api.github.com/search/users?q=${searchValue}`, {
+    headers: {
+      'Accept': 'application/vnd.github.v3+json'
+    }
+  })
     .then(response => response.json())
     .then(data => {
-      console.log(data); 
+      const userList = document.getElementById('user-list');
+      userList.innerHTML = ''; 
 
-      const userList = document.querySelector('#user-list');
-
-      userList.innerHTML = '';
-
-      data.items.forEach(user => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-          <img src="${user.avatar_url}" alt="${user.login}" width="50" height="50">
-          <a href="${user.html_url}" target="_blank">${user.login}</a>
-        `;
-        userList.appendChild(li);
-
-        li.addEventListener('click', function() {
-          getUserRepos(user.login);
-        });
+      const users = data.items; 
+      users.forEach(user => {
+        const listItem = document.createElement('li');
+        listItem.textContent = user.login;
+        userList.appendChild(listItem);
       });
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+    });
 });
 
-function getUserRepos(username) {
-  fetch(`https://api.github.com/users/${username}/repos`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); 
 
-      const reposList = document.querySelector('#repos-list');
 
-      reposList.innerHTML = '';
+// Second fucntion to create the elements and render it to the DOM
 
-      data.forEach(repo => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-          <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-          <p>${repo.description}</p>
-        `;
-        reposList.appendChild(li);
-      });
-    })
-    .catch(error => console.error(error));
-}
-});
-});
+
+
+
+
+
+})
